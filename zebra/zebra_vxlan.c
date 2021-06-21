4d62392e1c2905df3f364e030e7909db62c54dd2
Xiao Liang <shaw.leon@gmail.com>
2021-06-21 15:11:24 +0800

    zebra: Fix vxlan ns handling

--- zebra/zebra_vxlan.c
+++ zebra/zebra_vxlan.c
@@ -4497,8 +4497,7 @@ int zebra_vxlan_add_del_gw_macip(struct interface *ifp, const struct prefix *p,
 		/*
 		 * for a MACVLAN interface the link represents the svi_if
 		 */
-		svi_if = if_lookup_by_index_per_ns(zebra_ns_lookup(NS_DEFAULT),
-						   ifp_zif->link_ifindex);
+		svi_if = ifp_zif->link;
 		if (!svi_if) {
 			zlog_debug("MACVLAN %s(%u) without link information",
 				   ifp->name, ifp->ifindex);
@@ -4514,9 +4513,7 @@ int zebra_vxlan_add_del_gw_macip(struct interface *ifp, const struct prefix *p,
 
 			svi_if_zif = svi_if->info;
 			if (svi_if_zif) {
-				svi_if_link = if_lookup_by_index_per_ns(
-					zebra_ns_lookup(NS_DEFAULT),
-					svi_if_zif->link_ifindex);
+				svi_if_link = svi_if_zif->link;
 				zevpn = zebra_evpn_from_svi(svi_if,
 							    svi_if_link);
 			}
@@ -4535,9 +4532,7 @@ int zebra_vxlan_add_del_gw_macip(struct interface *ifp, const struct prefix *p,
 
 		svi_if_zif = ifp->info;
 		if (svi_if_zif) {
-			svi_if_link = if_lookup_by_index_per_ns(
-				zebra_ns_lookup(NS_DEFAULT),
-				svi_if_zif->link_ifindex);
+			svi_if_link = svi_if_zif->link;
 			if (svi_if_link)
 				zevpn = zebra_evpn_from_svi(ifp, svi_if_link);
 		}
@@ -4736,8 +4731,7 @@ void zebra_vxlan_macvlan_down(struct interface *ifp)
 	link_zif = link_ifp->info;
 	assert(link_zif);
 
-	link_if = if_lookup_by_index_per_ns(zebra_ns_lookup(NS_DEFAULT),
-					    link_zif->link_ifindex);
+	link_if = link_zif->link;
 
 	zl3vni = zl3vni_from_svi(link_ifp, link_if);
 	if (zl3vni) {
@@ -4766,8 +4760,7 @@ void zebra_vxlan_macvlan_up(struct interface *ifp)
 	link_zif = link_ifp->info;
 	assert(link_zif);
 
-	link_if = if_lookup_by_index_per_ns(zebra_ns_lookup(NS_DEFAULT),
-					    link_zif->link_ifindex);
+	link_if = link_zif->link;
 	zl3vni = zl3vni_from_svi(link_ifp, link_if);
 	if (zl3vni) {
 		/* associate with macvlan (VRR) interface */
