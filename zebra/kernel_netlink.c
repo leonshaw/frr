476196871bd644ccc988ad800ba3c6b8bb295388
Xiao Liang <shaw.leon@gmail.com>
2024-09-06 18:49:49 +0800

    zebra: Fix netlink buffer use after free
    With netns as VRF backend, kernel_terminate() is called for every netns
    being deleted. Move freeing of nl_batch_tx_buf to where zebra is really
    terminating.
    
    Signed-off-by: Xiao Liang <shaw.leon@gmail.com>

--- zebra/kernel_netlink.c
+++ zebra/kernel_netlink.c
@@ -1996,11 +1996,8 @@ void kernel_terminate(struct zebra_ns *zns, bool complete)
 	/* During zebra shutdown, we need to leave the dataplane socket
 	 * around until all work is done.
 	 */
-	if (complete) {
+	if (complete)
 		kernel_nlsock_fini(&zns->netlink_dplane_out);
-
-		XFREE(MTYPE_NL_BUF, nl_batch_tx_buf);
-	}
 }
 
 /*
@@ -2024,6 +2021,8 @@ void kernel_router_terminate(void)
 
 	hash_free(nlsock_hash);
 	nlsock_hash = NULL;
+
+	XFREE(MTYPE_NL_BUF, nl_batch_tx_buf);
 }
 
 #endif /* HAVE_NETLINK */
